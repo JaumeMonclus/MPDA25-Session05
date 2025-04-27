dd631c67807b9dc987a268f66b68039afe5c3e4b
Jaume Monclús <jaumemonclus7@gmail.com>
2025-04-27 12:19:40 +0200

    Shortest Path final delivery

---
+++ graphs/graphs--stripper-custon-JaumeMonclús.py
@@ -0,0 +1,133 @@
+#r: networkx
+#r: matplotlib
+
+# from ast import pattern
+from os import path, remove
+import re
+from typing import cast, Any
+from unittest.mock import seal
+import networkx as nx # type: ignore
+import matplotlib.pyplot as plt # type: ignore
+import Rhino.Geometry as rg
+import numpy as np
+from numpy import true_divide
+from System.Collections.Generic import List
+
+
+
+import ghpythonlib.treehelpers as th # type: ignore
+import ghpythonlib.components as comps # type: ignore
+import graph_helpers as graph 
+from System.Collections.Generic import List
+
+
+m = cast(rg.Mesh, m)  # type: ignore
+p = cast(rg.Point3d, p)
+
+
+
+class PathStripper:
+
+    def __init__(self, mesh, paths, points):
+
+        self.mesh = mesh
+        self.paths_tree   = paths
+        self.indexes = []
+        self.Polylines = []
+        self.points = points
+        self.cull_pattern = []
+
+    def compute_indexes(self):             
+        
+        self.indexes      = []
+        self.cull_pattern = []
+
+        for path in self.paths_tree.Paths:
+            branch = self.paths_tree.Branch(path)
+            if len(branch) >= 8:
+                self.indexes.append(branch)
+                self.cull_pattern.append(True)
+            else:
+                self.cull_pattern.append(False)
+
+        return self.indexes
+
+
+
+    def get_filtered_lists(self):
+        
+            if not self.cull_pattern:
+                self.compute_indexes()
+
+            filtered = []
+            for keep, path in zip(self.cull_pattern, self.points.Paths):
+                if not keep:
+                    continue
+                branch = self.points.Branch(path)
+                if branch:
+                    filtered.append(branch)
+
+            return filtered
+
+
+    def compute_polylines(self):
+        
+            self.polylines = []
+            for raw_branch in self.get_filtered_lists():
+                pts = [pt for pt in raw_branch if isinstance(pt, rg.Point3d)]
+                if pts:
+                    self.polylines.append(rg.Polyline(pts))
+            return self.polylines
+    
+    def compute_distances(self):
+
+        self.distances = []
+        self.endpoint_distances = []
+        self.checked_distances = []
+
+
+        for polyline in self.polylines:
+            length = polyline.Length
+            self.distances.append(length)
+
+        for polyline in self.polylines:
+            start, end = comps.EndPoints(polyline)
+            line = rg.Line(start, end)
+            distance = line.Length
+            self.endpoint_distances.append(distance)
+        
+        for i in range(len(self.distances)):
+            self.checked_distances.append(self.distances[i] - self.endpoint_distances[i])
+            
+        return self.checked_distances
+    
+    def compute_sorting(self):
+
+        paired = list(zip(self.checked_distances, self.indexes))
+        paired_sorted = sorted(paired, key=lambda x: x[0])
+
+        sorted_distances, sorted_polylines = zip(*paired_sorted) if paired_sorted else ([], [])
+
+        self.checked_distances = list(sorted_distances)
+        self.ordered_indexes   = list(sorted_polylines)
+
+        return self.distances
+    
+    def unpack_first_branch(self):
+
+        self.list_lengths   = [len(branch) for branch in self.ordered_indexes]
+        self.flattened_indexes = [item for branch in self.ordered_indexes for item in branch]
+
+        first_len = self.list_lengths[0]
+        first_branch = self.flattened_indexes[:first_len]
+
+        return first_branch
+
+
+a       = PathStripper(m, s, p)
+compute_indexes = a.compute_indexes()
+get_filtered_lists = a.get_filtered_lists()
+compute_polylines = a.compute_polylines()
+compute_distances = a.compute_distances()
+compute_sorting = a.compute_sorting()
+unpack_branches_to_lists = a.unpack_first_branch()
